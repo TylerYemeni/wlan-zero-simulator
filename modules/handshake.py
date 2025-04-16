02a811fe3e8847332ee443b82f31dd385581143a
TylerYemeni <leomessi111909@gmail.com>
2025-04-16 13:39:16 +0300

    Update handshake.py

--- modules/handshake.py
+++ modules/handshake.py
@@ -1,5 +1,17 @@
 import subprocess
 
-def start_sniffing(interface):
-    print("[+] Starting airodump-ng")
-    subprocess.run(["airodump-ng", interface])
+def capture_handshake(interface, bssid, channel):
+    try:
+        print(f"[+] التبديل إلى القناة {channel}...")
+        subprocess.run(["iwconfig", interface, "channel", str(channel)])
+        
+        print(f"[+] بدء التقاط Handshake من {bssid} ...")
+        subprocess.run([
+            "airodump-ng", 
+            "--bssid", bssid, 
+            "-c", str(channel), 
+            "-w", "capture", 
+            interface
+        ])
+    except Exception as e:
+        print(f"[!] خطأ أثناء التقاط Handshake: {e}")
